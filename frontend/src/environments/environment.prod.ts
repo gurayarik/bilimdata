8245d4f7d70358c7c45e4318ed7645fcaf8b6407
guray.arik <guray.arik@saglik.gov.tr>
2026-09-05 18:00:48 +0300

    .

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseUrl: 'https://xkaanbghqhgjecsepung.supabase.co',
   supabaseAnonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrYWFuYmdocWhnamVjc2VwdW5nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1ODA5NTIsImV4cCI6MjEwMzE1Njk1Mn0.G3dZk4heMHP17pI5SFidjNtIQ-A730fAs0XwzW4dJuU',
-  apiBaseUrl: 'https://bilimdata.up.railway.app',
+  apiBaseUrl: 'https://bilimdata-api.up.railway.app',
 };
